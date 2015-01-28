39f98cc7b7a78efa4ff90ccc4c81e2a661f0fda4
void-inc <void_inc@outlook.com>
2015-01-28 12:37:42 +0100

    first

---
+++ Rat/Assets/LeapMotion/Leap_Boilerplate/scripts/knife.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class knife : MonoBehaviour {
+
+
+	public GameObject target;
+	public float speed = 1f;
+	public float maxLifeTime = 0f;
+	private float lifeTime = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float delta = Time.deltaTime;
+		
+		
+		lifeTime += delta;
+		if (lifeTime > maxLifeTime)
+		{
+			Destroy(this.gameObject);
+		}
+		transform.Translate(Vector3.forward * speed * delta);   
+	}
+	
+	void OnCollisionEnter(Collision coll)
+	{
+		if(coll.gameObject.name == ("draairat"))
+		   {
+			Debug.Log ("hit!");
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Rat/Assets/LeapMotion/Leap_Boilerplate/scripts_for_example_scenes/WorldMover.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldMover : MonoBehaviour {
+	private LeapManager _leapManager;
+	public float shootspeed = 2f;
+	Rigidbody rb;
+	bool shootnow = false;
+	Vector3 nozzle;
+
+
+	public GameObject knife;
+	private GameObject direction;
+
+
+
+	// Use this for initialization
+	void Start () {
+		_leapManager = (GameObject.Find("LeapManager")as GameObject).GetComponent(typeof(LeapManager)) as LeapManager;
+		rb = gameObject.GetComponent<Rigidbody>();
+
+		Transform[] transforms = GetComponentsInChildren<Transform>();
+		foreach (Transform t in transforms)
+		{
+			if(t.gameObject.name == "direction")
+			{
+				direction = t.gameObject;
+			}
+		
+		}
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+		Quaternion rotation = Quaternion.Euler(Vector3.down * direction.transform.eulerAngles.y);
+
+
+
+		if (shootnow == true)
+		{
+			Instantiate(knife, direction.transform.position, rotation);
+		}
+
+
+		if (rb.transform.position.z >= shootspeed) {
+						shootnow = true;
+				} else {
+			shootnow = false;
+				}
+
+		if(_leapManager != null) { 
+			if(_leapManager.pointerAvailible)
+			{
+				this.transform.position = _leapManager.pointerPositionWorld;
+				if(!renderer.enabled) { renderer.enabled = true; }
+			}
+			else
+			{
+				renderer.enabled = false;
+			}
+		}
+	}
+}
